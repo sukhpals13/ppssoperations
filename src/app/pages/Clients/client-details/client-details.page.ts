96173341ed1781d7cfcd9f7d6a41ed423385342d
Abhishek <abhishek@v-admins-Mac-mini-3.local>
2019-09-27 11:45:35 +0530

    New client details page

--- src/app/pages/Clients/client-details/client-details.page.ts
+++ src/app/pages/Clients/client-details/client-details.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { GetDetailsService } from '../../../services/getDetails/get-details.service';
 
 @Component({
   selector: 'app-client-details',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientDetailsPage implements OnInit {
 
-  constructor() { }
+  public client : any;
+
+  @HostBinding('class.is-shell') get isShell() {
+    return (this.client && this.client.isShell) ? true : false;
+  }
+
+  constructor(
+    private getDetailsService: GetDetailsService,
+  ) { }
 
   ngOnInit() {
+    this.client = {
+      name:null,
+      customizations:{
+
+      },
+      ranksOrTitles:[null,null],
+      assignments:[null,null],
+      isShell: true
+    };
+  }
+  ionViewDidEnter(){
+    this.getDetailsService.getClient().subscribe(res=>{
+      console.log(res);
+      this.client = res.client;
+    },err=>{
+      console.log(err);
+    })
   }
 
 }
--- src/app/services/getDetails/get-details.service.ts
+++ src/app/services/getDetails/get-details.service.ts
@@ -29,4 +29,9 @@ export class GetDetailsService {
   getAllClients(searchText: string){console.log('hello',searchText);
     return this.http.post<ClientsListModel>('https://integration.ebusiness.pittsburghpublicsafety.com/api/client/find',{"searchPhrase":searchText});
   }
+  
+  // Get Client Details https://integration.ebusiness.pittsburghpublicsafety.com/api/client/5d55a0a8951e360004cf8d2a
+  getClient(){
+    return this.http.get<any>('https://integration.ebusiness.pittsburghpublicsafety.com/api/client/5d55a0a8951e360004cf8d2a')
+  }
 }
