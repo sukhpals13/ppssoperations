1840fe2fb203c02981bd17bb358b82e4f0cc6c88
sukhpals13 <sukhpal.singh@mail.valuecoders.com>
2019-09-30 15:18:25 +0530

    client-details

--- src/app/pages/Clients/client-details/client-details.page.ts
+++ src/app/pages/Clients/client-details/client-details.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
 import { GetDetailsService } from '../../../services/getDetails/get-details.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { PostDetailsService } from '../../../services/postDetails/post-details.service';
 
 @Component({
   selector: 'app-client-details',
@@ -15,6 +17,7 @@ export class ClientDetailsPage implements OnInit {
   public rankAddition: string;
   public assingmentAddition: string;
   public storedData: any;
+  alertCtrl: any;
 
   @HostBinding('class.is-shell') get isShell() {
     return (this.client && this.client.isShell) ? true : false;
@@ -22,7 +25,10 @@ export class ClientDetailsPage implements OnInit {
 
   constructor(
     private getDetailsService: GetDetailsService,
+    private postDetailsService: PostDetailsService,
     private _Activatedroute: ActivatedRoute,
+    public alertController: AlertController
+    
     // private router: Router,
   ) { }
 
@@ -47,7 +53,8 @@ export class ClientDetailsPage implements OnInit {
     // console.log(this._Activatedroute,'this._Activatedroute');
     // console.log(this.router,'this.router');
     this._Activatedroute.params.subscribe(it => {
-      clientId = it.cNumber;
+      clientId = it.cNumber; 
+      this.clientId = it.cNumber; 
     })
     console.log(clientId);
     this.getDetailsService.getClient(clientId).subscribe(res=>{
@@ -63,19 +70,81 @@ export class ClientDetailsPage implements OnInit {
   }
   addRanks(){
     this.client.ranksOrTitles.push(this.rankAddition);
-    this.rankAddition = '';
+    // this.rankAddition = '';
+     this.postDetailsService.addRank(this.clientId,this.rankAddition).subscribe(res=>{
+      console.log(res);
+
+    },err=>{
+      console.log(err);
+    })
   }
+  
   addAssingment(){
     this.client.assignments.push(this.assingmentAddition);
-    this.assingmentAddition = '';
+    // this.assingmentAddition = '';
+
+    this.postDetailsService.addAssignment(this.clientId, this.assingmentAddition).subscribe(res=> {
+      console.log(res);
+      
+    }, err=> {
+      console.log(err);
+      
+    }
+    
+    )
   }
-  deleteRank(r){
+ async deleteRank(r){
     // console.log(r);
-    this.client.ranksOrTitles = this.client.ranksOrTitles.filter(v=>v!==r);
+    // this.client.ranksOrTitles = this.client.ranksOrTitles.filter(v=>v!==r);
+    const alert = await this.alertController.create({
+      header: 'Confirm!',
+      message: 'Are you sure you want to delete the Rank or Titles?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            // console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Delete',
+          handler: () => {
+            console.log('Confirm Okay');
+            this.client.ranksOrTitles = this.client.ranksOrTitles.filter(v=>v!==r);
+          }
+        }
+      ]
+    });
+    await alert.present();
+    
   }
-  deleteAssignment(a){
+  async deleteAssignment(a){
     // console.log(a);
-    this.client.assignments = this.client.assignments.filter(v=>v!==a);
+    // this.client.assignments = this.client.assignments.filter(v=>v!==a);
+
+    const alert = await this.alertController.create({
+      header: 'Confirm!',
+      message: 'Are you sure you want to delete the Assingment?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Delete',
+          handler: () => {
+            console.log('Confirm Okay');
+            this.client.ranksOrTitles = this.client.ranksOrTitles.filter(v=>v!==a);
+          }
+        }
+      ]
+    });
+    await alert.present();
+
   }
   
 
