256ce2fe78816e91e8657353e4965ab57da6f4b7
Amit Kumar <amit.kumar1@mail.valuecoders.com>
2019-09-30 14:35:17 +0530

    Client billing detail service

--- src/app/services/postDetails/post-details.service.ts
+++ src/app/services/postDetails/post-details.service.ts
@@ -38,5 +38,11 @@ export class PostDetailsService {
     let url ="https://integration.ebusiness.pittsburghpublicsafety.com/api/client/rank/"+clientId;
     return this.http.put<any>(url,{name:rank})
   }
+  updateBillingInfo(clientId,reqBody){
+    let url ="https://integration.ebusiness.pittsburghpublicsafety.com/api/client/updateBilling/"+'5d40606c480c470004320f76';
+    return this.http.post<any>(url,reqBody)
+  }
+
+  // /api/client/updateBilling/5d40606c480c470004320f76
 
 }
